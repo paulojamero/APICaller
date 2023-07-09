ae7c0e0565b6a055242675b100869e5afa3d3293
paulojamero <jameropaulo07@yahoo.com>
2023-07-10 06:16:02 +0800

    Add project files.

---
+++ APICaller/GetCatBreed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace APICaller
+{
+    public class GetCatBreed
+    {
+        private readonly HttpClient _httpClient;
+
+        public GetCatBreed()
+        {
+             _httpClient = new HttpClient();
+        }
+
+        public async Task BreedCaller(int numLimit)
+        {
+            string apiUrl = $"https://catfact.ninja/breeds?limit={numLimit}";
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.Write(responseBody);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"An Error occured {ex.Message}.");
+            }
+            finally
+            {
+                _httpClient.Dispose();
+            }
+        }
+
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using APICaller;
+
+namespace TestConsole
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GetCatBreed getCatBreed = new GetCatBreed();
+            Console.WriteLine("Enter limit: ");
+            int numLimit = Convert.ToInt32(Console.ReadLine());
+
+
+            await getCatBreed.BreedCaller(numLimit);
+
+            Console.ReadLine();
+        }
+    }
+}
